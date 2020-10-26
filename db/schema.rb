05af8f67309d4122fb0e558e74e39843af242c94
souta3945884 <s.k_stbsk.r12@docomo.ne.jp>
2020-10-26 14:59:47 +0000

    表記を変えるため保存

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_24_032517) do
+ActiveRecord::Schema.define(version: 2020_10_26_053145) do
 
   create_table "topics", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_10_24_032517) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "image_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
